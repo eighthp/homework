9bd4cf06e95f72a569f3d08d5578053e1dd1c6b1
Almin Grigoriy <eighthp@hotmail.com>
2024-01-23 15:48:24 +0200

    Initial commit

---
+++ homework2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+    class Rectangle {
+        float m_a;
+        float m_b;
+    public:
+        Rectangle() = default;
+        Rectangle(float a, float  b)
+        {
+            setA(a);
+            setB(b);
+        }
+        void setA(float a)
+        {
+            if (a < 0)
+                std::cout << "Negative";
+            m_a=a;
+
+        }
+        void setB(float b)
+        {
+            if (b < 0)
+                std::cout << "Negative";
+            m_b=b;
+        }
+
+        float Area()
+        {
+            return m_a*m_b;
+        }
+
+        float Perimeter()
+        {
+            return (m_a+m_b)*2;
+        }
+
+        float Diagonal()
+        {
+            return sqrt(pow(m_a,2) + pow(m_b,2));
+        }
+    };
+    Rectangle r(2,1);
+    r.setA(3);
+    r.setB(4);
+    std::cout << "Area = " << r.Area() << std::endl;
+    std::cout << "Perimeter = " << r.Perimeter() << std::endl;
+    std::cout << "Diagonal = " << r.Diagonal() << std::endl;
+
+    return 0;
+}
